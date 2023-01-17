51934dc0a6c8138105b742cbdfeab2443f2117e9
Iris Bromley <iris.j.bromley@gmail.com>
2023-01-17 15:14:12 +0100

    Add style

--- src/App.js
+++ src/App.js
@@ -3,31 +3,53 @@ import { useState } from 'react';
 export const useGenerateRandomColor = () => {
   const [color, setColor] = useState('');
   const generateColor = () => {
-    setColor(Math.random().toString(16).substr(-6));
+    // make a random number and use the last 6 digits
+    setColor(Math.random().toString(16).slice(-6));
   };
   return { color, generateColor };
 };
 export default function App() {
-  const { color, generateColor } = useGenerateRandomColor();
+  const { color, generateColor } = useGenerateRandomColor('');
   console.log(color);
   return (
     <div>
-      <button
-        onClick={() => {
-          generateColor();
-        }}
-      >
-        Generate
-      </button>
       <div
         style={{
           backgroundColor: '#' + color,
           width: 150,
           height: 150,
           margin: 'auto',
+          marginTop: '200px',
+          borderRadius: '7px',
+          border: '1px solid #00000030',
         }}
       >
-        <p>Generated color: {'#' + color}</p>
+        <p style={{ textAlign: 'center' }}>
+          Generated color: <br /> {'#' + color}
+        </p>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          paddingTop: '20px',
+        }}
+      >
+        <button
+          style={{
+            border: '1px solid #00000030',
+            borderRadius: '7px',
+            textAlign: 'center',
+            width: 150,
+            height: 30,
+            backgroundColor: '##cdb9b914',
+          }}
+          onClick={() => {
+            generateColor();
+          }}
+        >
+          Generate
+        </button>
       </div>
     </div>
   );
